9dd530532342fc402b5ad007c5f13eba313cc1a9
Kaijun <Demonbane.cn@gmail.com>
2016-04-16 22:36:14 +0200

    .

--- 001-Two_Sum.js
+++ 001-Two_Sum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-	"use strict"
-	let numsHash = {}
-	let results = []
-	nums.forEach((item, idx) => {
-		let diff = target - item
-		if(numsHash[diff] !== undefined)
-			results = [idx, numsHash[diff]]
-		numsHash[item] = idx
-	})
-	return results
+  "use strict"
+  let numsHash = {}
+  let results = []
+  nums.forEach((item, idx) => {
+    let diff = target - item
+    if(numsHash[diff] !== undefined)
+      results = [idx, numsHash[diff]]
+    numsHash[item] = idx
+  })
+  return results
 }
