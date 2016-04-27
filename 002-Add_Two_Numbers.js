7438e4df4b5ab531facd0a735017baceda081965
Kaijun <Demonbane.cn@gmail.com>
2016-04-27 21:54:16 +0200

    #002

--- 002-Add_Two_Numbers.js
+++ 002-Add_Two_Numbers.js
@@ -11,6 +11,18 @@
  * @return {ListNode}
  */
 const addTwoNumbers = (l1, l2) => {
-  'use strict'
-  
-}
+    'use strict';
+    let l = new ListNode(-1)
+    let result = l
+    let up = 0
+    while(l1 || l2 || up!==0 ){
+        let digit = parseInt(l1?l1.val:0) + parseInt(l2?l2.val:0) + up
+        up = digit>=10?1:0
+        digit = digit>=10?digit%10:digit
+        l1 = l1?l1.next:null
+        l2 = l2?l2.next:null
+        l.next = new ListNode(digit)
+        l=l.next
+    }
+    return result.next
+};
